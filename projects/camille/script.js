1936dad59929748bb328c3c29deccc4b112f1836
Evan <evanyui@hotmail.com>
2016-09-18 07:36:27 -0400

    readd projects

---
+++ projects/camille/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  // tasks to do if it is a Mobile Device
+  $('smoke').css('margin-top','25%');
+  }
+});
