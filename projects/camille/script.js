a0943a6afc9c9d4b5b93e2a5997f49ba2c0f9aaf
Chuanyui Teh <evanyui@hotmail.com>
2019-07-13 14:39:21 -0700

    use as a2z app

--- projects/camille/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  // tasks to do if it is a Mobile Device
-  $('smoke').css('margin-top','25%');
-  }
-});
